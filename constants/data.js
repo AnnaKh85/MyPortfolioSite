09ece80e6a71e07bf051921f3b6153221287eff5
Anna <AKhizchnyakova@1cbit.ru>
2023-01-14 10:15:50 +0300

    init commint

---
+++ constants/data.js
@@ -0,0 +1,118 @@
+const userData = {
+  githubUsername: "AnnaKh85",
+  name: "Anna Khizhnyakova",
+  designation: "Analyst, QA and runner",
+  avatarUrl: "/avatar.jpg",
+  email: "anna.Khizhnyakova@gmail.com",
+  phone: "+123456789",
+  address: "Russia, Moscow",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "Я просто аналитик 1С, не совсем простой, ведущий, но кажется мне этого мало и я решила стать больше чем просто аналитик. Хочу понимать и применять на практике, как работают другие проекты, как тестируются приложения и пр.",
+    description: [
+      `Я закончила Тульский государственный университет, факультет кибернетики. Какое то время работала программистом, но позже потянуло на аналитику, нашла работу в большой 
+      франчайзинговой компании. Сначала просто ездила и устанавливала клиентам 1С, немного программировала (небольшие доработки прям у клиента в офисе), потом перешла в проектный отдел
+      и стала аналитиком. Потом ведущим аналитиком, потом руководителем проектов.`,
+      `Но я никгда не переставала учиться - курсы, сертификаты, семинары, сначала по 1С, а сейчас интересует все из реального WebDev, технологии разработки и вредрения продуктов.`,
+      `Но еще больше меня интересют процессы обеспечения качества продукта - от составления документации, тест кейсов, мануалього тестирования до автоматизации. 
+      В своем маленьком pet-проекте я использую такой набор инструментов - React, Next.js, MySQL, Tailwind, Netlify, Cypress, git, ClickUp, GitBook.. и возможно что то еще забыла`,
+    ],
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "mroads",
+      year: "2021",
+      companyLink: "https://mroads.com",
+      desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
+    },
+    {
+      title: "Founder and Developer",
+      company: "TailwindMasterKit",
+      year: "2021",
+      companyLink: "https://tailwindmasterkit.com",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Founder and Developer",
+      company: "Algochurn",
+      year: "2021",
+      companyLink: "https://algochurn.com",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Internship",
+      company: "Intel Corporation",
+      year: "2019",
+      companyLink: "https://intel.com",
+      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+    },
+    {
+      title: "Graduation",
+      company: "UPES, Dehradun.",
+      year: "2019",
+      companyLink: "https://upes.co.in",
+      desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "Delhi Public School, Jaipur.",
+      year: "2015",
+      companyLink: "https://upes.co.in",
+      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://1drv.ms/b/s!AlWNzi4oJ48CeB-52PkpyTtWMWA?e=bbyWfq",
+  socialLinks: {
+    instagram: "https://www.instagram.com/anna_khizhnyakova/",
+    //twitter: "https://twitter.com/mannupaaji",
+    linkedin: "https://www.linkedin.com/in/anna-kh/",
+    github: "https://github.com/AnnaKh85",
+    facebook: "https://www.facebook.com/khz.anna",
+  },
+};
+
+export default userData;
